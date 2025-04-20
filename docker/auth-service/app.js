4b8d71866c583350ea6091f4d6920ac6b06f5c3e
Pedro <76980297+PedroHenriqueDutra@users.noreply.github.com>
2025-04-20 16:18:33 -0300

    Add files via upload

---
+++ docker/auth-service/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { Client } = require('pg');
+const app = express();
+const port = 3000;
+
+// Conectar ao banco de dados PostgreSQL
+const client = new Client({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: 5432,
+});
+
+client.connect()
+  .then(() => console.log("Conectado ao banco de dados PostgreSQL"))
+  .catch(err => console.error("Erro ao conectar ao banco de dados", err));
+
+// Definir uma rota simples para o serviço
+app.get('/', (req, res) => {
+  res.send('Auth Service is Running');
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Auth Service running at http://localhost:${port}`);
+});
